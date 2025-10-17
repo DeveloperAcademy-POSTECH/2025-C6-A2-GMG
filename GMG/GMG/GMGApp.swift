db188a359770c369bdb3e16dbb7a1ba4b08f4c39
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-10-17 15:05:42 +0900

    [chore] Xcode Project

---
+++ GMG/GMG/GMGApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMGApp.swift
+//  GMG
+//
+//  Created by 나현흠 on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GMGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
