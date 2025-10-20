fdbc7a5f0c1ed84149afeb46e4f1e924f1c39ce1
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-10-20 10:43:11 +0900

    [FEAT] Fastlane 설정
    - Matchfile, Fastfile을 업데이트 했습니다.
    - 인증서 발급, 기기 등록이 가능합니다.
    - 명령어 및 정리본은 노션에 첨부했습니다.

--- GMG/GMG/Presentation/ContentView.swift
+++ GMG/GMG/Presentation/ContentView.swift
@@ -14,8 +14,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-                .font(.dosGothic(.m13))
-                .foregroundStyle(Color.light)
         }
         .padding()
     }
