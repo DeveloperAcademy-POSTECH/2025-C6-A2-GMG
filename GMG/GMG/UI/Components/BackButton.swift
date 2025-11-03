29a44be3b58fe8ddd38a44fe051c02934707989d
Huigyun Jeong <topone03@gmail.com>
2025-11-03 21:06:33 +0900

    [feat] implement custom navigation bar and back button components

---
+++ GMG/GMG/UI/Components/BackButton.swift
@@ -0,0 +1,20 @@
+//  Copyright © 2025 ADA 4th GMG. All rights reserved.
+
+import SwiftUI
+
+struct BackButton: View {
+    @Environment(\.dismiss) private var dismiss
+
+    var body: some View {
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "chevron.backward")
+                .foregroundStyle(Color.Text.black)
+        }
+    }
+}
+
+#Preview {
+    BackButton()
+}
