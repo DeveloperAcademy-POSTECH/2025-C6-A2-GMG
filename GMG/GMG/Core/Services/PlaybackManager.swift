38d8bee70ab48daeb0cb95c88559df3b66857195
Huigyun Jeong <topone03@gmail.com>
2025-11-03 21:07:06 +0900

    [feat] add AudioConductor, PlaybackManager, and RecordManager services

---
+++ GMG/GMG/Core/Services/PlaybackManager.swift
@@ -0,0 +1,50 @@
+//
+//  PlaybackManager.swift
+//  SwiftF0
+//
+//  Created by 정희균 on 10/28/25.
+//
+
+import AudioKit
+import Combine
+import Foundation
+
+@Observable
+final class PlaybackManager {
+    private var audioPlayer: AudioPlayer
+    
+    private(set) var isPlayingPublisher: CurrentValueSubject<Bool, Never>
+
+    init() {
+        self.audioPlayer = AudioPlayer()
+
+        self.isPlayingPublisher = CurrentValueSubject<Bool, Never>(false)
+        
+        self.audioPlayer.completionHandler = { [weak self] in
+            guard let self else { return }
+            self.isPlayingPublisher.send(false)
+        }
+    }
+
+    func play(_ url: URL) throws {
+        try AudioConductor.shared.setAudioMode(.playback)
+        AudioConductor.shared.addOutput(audioPlayer)
+        try AudioConductor.shared.start()
+
+        try self.audioPlayer.load(url: url)
+        self.audioPlayer.play()
+        self.audioPlayer.isLooping = false
+        
+        self.isPlayingPublisher.send(true)
+    }
+
+    func stop() {
+        self.audioPlayer.stop()
+
+        AudioConductor.shared.stop()
+        AudioConductor.shared.removeOutput(audioPlayer)
+        try? AudioConductor.shared.setAudioMode(nil)
+        
+        self.isPlayingPublisher.send(false)
+    }
+}
