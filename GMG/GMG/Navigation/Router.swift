0e58a66f1485b537ce1a6b6a1cf5f9c043b4fea2
Huigyun Jeong <topone03@gmail.com>
2025-11-05 22:01:44 +0900

    [feat] implement Router and app entry point

--- GMG/GMG/Navigation/Router.swift
+++ GMG/GMG/Navigation/Router.swift
@@ -1,7 +1,45 @@
-//
-//  Router.swift
-//  GMG
-//
-//  Created by 나현흠 on 10/18/25.
-//
+//  Copyright © 2025 ADA 4th GMG. All rights reserved.
 
+import SwiftUI
+
+enum Route: Hashable {
+    case home
+    case recording
+    case chordProgress(score: Score)
+    case export
+}
+
+@Observable
+final class Router {
+    var path: NavigationPath
+    
+    init() {
+        self.path = NavigationPath()
+    }
+    
+    func push(_ route: Route) {
+        path.append(route)
+    }
+    
+    func pop() {
+        path.removeLast()
+    }
+    
+    func popToRoot() {
+        path = NavigationPath()
+    }
+    
+    @ViewBuilder
+    func view(_ route: Route) -> some View {
+        switch route {
+        case .home:
+            HomeView()
+        case .recording:
+            RecordingView()
+        case .chordProgress(let score):
+            ChordProgressView(score: score)
+        case .export:
+            ExportView()
+        }
+    }
+}
