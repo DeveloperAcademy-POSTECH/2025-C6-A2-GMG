1283927498c8e16f67b8a0470a46684536a8dca5
Huigyun Jeong <topone03@gmail.com>
2025-11-10 15:19:08 +0900

    [fix] navigation issue when swipe gesture doesn’t work

---
+++ GMG/Core/Utils/UINavigationController+Extensions.swift
@@ -0,0 +1,15 @@
+//  Copyright © 2025 ADA 4th GMG. All rights reserved.
+
+import SwiftUI
+
+/// Hide navigation bar without losing swipe back gesture
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
