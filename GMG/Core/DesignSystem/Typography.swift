3785cf4a8de4b14301949b5e9495c401e84117c1
Huigyun Jeong <topone03@gmail.com>
2025-11-10 23:24:02 +0900

    [style] add Pretendard font files and setup typography system

--- GMG/Core/DesignSystem/Typography.swift
+++ GMG/Core/DesignSystem/Typography.swift
@@ -106,4 +106,92 @@ enum Typography {
             }
         }
     }
+
+    enum Pretendard: CustomFont {
+        case R1
+        case M1
+        case M2
+        case M3
+        case M4
+        case M5
+        case M6
+        case M7
+        case M8
+        case M9
+        case M10
+        case M11
+        case M12
+        case M13
+        case M14
+        case M15
+        case M16
+        case SB1
+        case SB2
+        case SB3
+        case SB4
+        case SB5
+        case SB6
+        case SB7
+
+        static let Thin: String = "Pretendard-Thin"
+        static let ExtraLight: String = "Pretendard-ExtraLight"
+        static let Light: String = "Pretendard-Light"
+        static let Regular: String = "Pretendard-Regular"
+        static let Medium: String = "Pretendard-Medium"
+        static let SemiBold: String = "Pretendard-SemiBold"
+        static let Bold: String = "Pretendard-Bold"
+        static let ExtraBold: String = "Pretendard-ExtraBold"
+        static let Black: String = "Pretendard-Black"
+
+        var font: Font {
+            return Font.custom(self.fontName, size: self.size)
+        }
+
+        var fontName: String {
+            switch self {
+            case .R1:
+                return Self.Regular
+            case .M1, .M2, .M3, .M4, .M5, .M6, .M7, .M8,
+                .M9, .M10, .M11, .M12, .M13, .M14, .M15, .M16:
+                return Self.Medium
+            case .SB1, .SB2, .SB3, .SB4, .SB5, .SB6, .SB7:
+                return Self.SemiBold
+            }
+        }
+
+        var size: CGFloat {
+            switch self {
+            case .R1: return 14
+            case .M1: return 8
+            case .M2: return 10
+            case .M3: return 12
+            case .M4: return 14
+            case .M5: return 16
+            case .M6: return 18
+            case .M7: return 20
+            case .M8: return 22
+            case .M9: return 24
+            case .M10: return 26
+            case .M11: return 28
+            case .M12: return 30
+            case .M13: return 32
+            case .M14: return 34
+            case .M15: return 36
+            case .M16: return 38
+            case .SB1: return 8
+            case .SB2: return 10
+            case .SB3: return 12
+            case .SB4: return 14
+            case .SB5: return 16
+            case .SB6: return 18
+            case .SB7: return 20
+            }
+        }
+
+        var lineHeightMultiple: CGFloat {
+            switch self {
+            default: return 1.4
+            }
+        }
+    }
 }
