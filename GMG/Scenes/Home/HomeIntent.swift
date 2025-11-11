16ea773b6bfb32ef50c29a040f899d01319c4b6b
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-11-12 03:13:11 +0900

    [#39] feat: add HomeIntent

--- GMG/Scenes/Home/HomeIntent.swift
+++ GMG/Scenes/Home/HomeIntent.swift
@@ -1,2 +1,39 @@
 //  Copyright © 2025 ADA 4th GMG. All rights reserved.
 
+import Foundation
+
+protocol HomeIntentProtocol {
+    func loadScores()
+    func toggleSortOrder()
+    func deleteScore(_ score: Score)
+    func selectScore(_ score: Score?)
+    func renameScore(_ score: Score, newTitle: String)
+}
+
+final class HomeIntent: HomeIntentProtocol {
+    private var model: HomeModelActionProtocol?
+    
+    init(model: HomeModelActionProtocol) {
+        self.model = model
+    }
+    
+    func loadScores() {
+        model?.fetchScores()
+    }
+    
+    func toggleSortOrder() {
+        model?.toggleIsLatest()
+    }
+    
+    func deleteScore(_ score: Score) {
+        model?.deleteScore(score)
+    }
+    
+    func selectScore(_ score: Score?) {
+        model?.setSelectedScore(score)
+    }
+    
+    func renameScore(_ score: Score, newTitle: String) {
+        model?.renameScore(score, newTitle: newTitle)
+    }
+}
