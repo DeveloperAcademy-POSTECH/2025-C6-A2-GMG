073094cb36d72a26201ecfce0662c4988bbd39e9
ehdrbdndns <ehdrbdndns@naver.com>
2025-11-11 12:19:03 +0900

    [refactor] Ensures model is available before use

--- GMG/Scenes/Recording/RecordingIntent.swift
+++ GMG/Scenes/Recording/RecordingIntent.swift
@@ -147,7 +147,8 @@ final class RecordingIntent: RecordingIntentProtocol {
         scoreCreationTask?.cancel()
 
         scoreCreationTask = Task { [weak self] in
-            guard let self else { return }
+            guard let self, let model = self.model
+            else { return }
 
             do {
                 // TODO: Swift Concurrency 방법 찾아보기!
@@ -165,13 +166,14 @@ final class RecordingIntent: RecordingIntentProtocol {
                     }
                 }
 
-                self.model?.updateScoreFactoryState(nil)
-                self.model?.finishScoreCreation(score)
+                model.updateScoreFactoryState(nil)
+                model.finishScoreCreation(score)
                 completion()
             } catch {
-                self.model?.updateScoreFactoryState(nil)
+                model.updateScoreFactoryState(nil)
                 Logger.error(String(describing: error))
             }
         }
     }
 }
+
