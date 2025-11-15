c9a6108e7091febaa8293c34f31ee690e2b1bd29
Huigyun Jeong <topone03@gmail.com>
2025-11-16 00:57:44 +0900

    [#75] feat: update custom `font` modifier for iOS 18 compatibility

--- GMG/Core/Utils/View+Extensions.swift
+++ GMG/Core/Utils/View+Extensions.swift
@@ -3,10 +3,17 @@
 import SwiftUI
 
 extension View {
+    @ViewBuilder
     func font(_ customFont: CustomFont) -> some View {
-        self
-            .font(customFont.font)
-            .lineHeight(.multiple(factor: customFont.lineHeightMultiple))
+        if #available(iOS 26.0, *) {
+            self
+                .font(customFont.font)
+                .lineHeight(.multiple(factor: customFont.lineHeightMultiple))
+        } else {
+            self
+                .font(customFont.font)
+                .lineSpacing(customFont.size * 0.2)
+        }
     }
 }
 
