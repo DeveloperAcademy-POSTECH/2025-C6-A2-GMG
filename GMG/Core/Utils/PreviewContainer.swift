629247312e7f504ee5d13d826e2cb5cd3013b440
Huigyun Jeong <topone03@gmail.com>
2025-11-16 22:30:52 +0900

    [#54] feat: add SwiftData sample data for preview

--- GMG/Core/Utils/PreviewContainer.swift
+++ GMG/Core/Utils/PreviewContainer.swift
@@ -3,6 +3,8 @@
 import SwiftUI
 
 struct PreviewContainer<Content: View>: View {
+    @State private var diContainer: DIContainer
+    @State private var router: Router
     private let content: Content
 
     init(
@@ -11,12 +13,37 @@ struct PreviewContainer<Content: View>: View {
         let diContainer: DIContainer = DIContainer(isStoredInMemoryOnly: true)
         let router: Router = Router(diContainer: diContainer)
 
+        self.diContainer = diContainer
+        self.router = router
+
         self.content = content(router)
+
+        prepareSampleData()
     }
 
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $router.path) {
             content
+                .navigationDestination(for: Route.self) { route in
+                    router.view(route)
+                }
+        }
+    }
+
+    private func prepareSampleData() {
+        do {
+            guard let scoreRepository: ScoreRepository = diContainer.makeScoreRepository() else {
+                Logger.warning("Failed to create score repository")
+                return
+            }
+
+            for _ in 0..<10 {
+                let score: Score = Score.mock
+
+                try scoreRepository.insert(score)
+            }
+        } catch {
+            Logger.error(String(describing: error))
         }
     }
 }
