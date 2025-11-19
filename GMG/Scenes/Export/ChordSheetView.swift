5d631b1386d242d0c923d548bfb47a85fcee53bf
Huigyun Jeong <topone03@gmail.com>
2025-11-19 23:48:24 +0900

    [#107] feat: add chord sheet

--- GMG/Scenes/Export/ChordSheetView.swift
+++ GMG/Scenes/Export/ChordSheetView.swift
@@ -6,7 +6,39 @@ struct ChordSheetView: View {
     let score: Score
 
     var body: some View {
-        Text("export text \n to sheet")
+        ZStack {
+            Color.white1
+                .ignoresSafeArea()
+
+            VStack {
+                VStack(spacing: Spacing.md) {
+                    Text(score.title)
+                        .font(.custom(Typography.WantedSansStd.Bold, size: 40))
+                    Text("\(score.key.description) Key")
+                        .font(Typography.WantedSansStd.R7)
+                }
+                .foregroundStyle(Color.black1)
+
+                VStack {
+
+                }
+                .frame(minHeight: 490)
+
+                Image(.sheetLogo)
+            }
+        }
+    }
+}
+
+extension ChordSheetView {
+    struct ChordCellView: View {
+        let chordCell: ChordCell
+        let duration: TimeInterval
+
+        var body: some View {
+            RoundedRectangle(cornerRadius: <#T##CGFloat#>)
+            Text(chordCell.chord?.description ?? "")
+        }
     }
 }
 
