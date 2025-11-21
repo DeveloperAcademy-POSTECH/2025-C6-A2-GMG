8ad41102e9a93d4b4e098e76713685956075f4f6
ehdrbdndns <ehdrbdndns@naver.com>
2025-11-21 20:05:36 +0900

    [#124] refactor: Adjusts chord candidates and tightens segment layout

--- GMG/Core/Models/Score/Score.swift
+++ GMG/Core/Models/Score/Score.swift
@@ -176,7 +176,7 @@ extension Score {
                 ChordCell(
                     chord: Chord(root: .C, quality: .maj),
                     chordCandidates: [
-                        Chord(root: .C, quality: .maj), Chord(root: .D, quality: .maj),
+                        Chord(root: .C, quality: .maj), Chord(root: .Gs, quality: .halfDim7),
                         Chord(root: .Ab, quality: .maj), Chord(root: .Gb, quality: .maj),
                         Chord(root: .Eb, quality: .maj),
                     ],
