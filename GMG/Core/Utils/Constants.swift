f3c00f01145b70edd697509a116855b65a33f300
n_joid <72697791+nakisara01@users.noreply.github.com>
2025-11-21 17:05:01 +0900

    [#78] fix: add Constants sampleRate & segmentDur

---
+++ GMG/Core/Utils/Constants.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2025 ADA 4th GMG. All rights reserved.
+
+import Foundation
+
+enum Constants {
+    /// ChordProgress에서 사용
+    static let segmentDuration: TimeInterval = 5.0
+    static let sampleRate: Double = 48_000
+}
